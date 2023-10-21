c0c7a933972dd9e11f51ebdf0d8cbd2375f18c82
hervey14 <hervey146@gmail.com>
2023-10-21 13:24:38 -0700

    API Push (Reajustando Metodo POST de Ventas) addVenta) 21 de octubre 2023

--- API/controllers/abarrotesController.js
+++ API/controllers/abarrotesController.js
@@ -395,49 +395,55 @@ function encontrarCategoriaPorId(id, categorias) {
   }
 };
 
-  // Función para agregar una nueva categoría
-  exports.addCategory = (req, res) => {
-    const { nombre } = req.body;
-  
-    // Validar los datos de la categoría
-    const requiredProperties = ['nombre'];
-  
-    if (
-      !requiredProperties.every(prop => req.body.hasOwnProperty(prop)) ||
-      Object.keys(req.body).length !== requiredProperties.length
-    ) {
-      return res.status(400).json({ error: 'Estructura incorrecta en el cuerpo de la solicitud' });
-    }
-  
-    if (!nombre) {
-      return res.status(400).json({ error: 'El nombre de la categoría es obligatorio' });
-    }
-  
-    const newCategory = new Categoria(generateUniqueId(), nombre);
-  
-    try {
-      // Leer datos actuales de categorías desde el archivo
-      const categoriasData = fs.readFileSync(filePath3, 'utf-8');
-      let categorias = JSON.parse(categoriasData);
-  
-      // Verificar si categorías es un array, si no, inicializar como array vacío
-      if (!Array.isArray(categorias)) {
-        categorias = [];
-      }
-  
-      // Agregar la nueva categoría al array de categorías
-      categorías.push(newCategory);
-  
-      // Escribir el array actualizado de categorías de nuevo al archivo
-      fs.writeFileSync(filePath3, JSON.stringify(categorias, null, 2));
-  
-      // Responder con la nueva categoría añadida
-      res.status(201).json(newCategory);
-    } catch (error) {
-      console.error('Error al agregar la categoría:', error);
-      res.status(500).json({ error: 'Error interno del servidor' });
-    }
+// Función para agregar una nueva categoría
+exports.addCategory = (req, res) => {
+  const { nombre } = req.body;
+
+  // Validar los datos de la categoría
+  const requiredProperties = ['nombre'];
+
+  // Excluir 'id' de la validación
+  const bodyProperties = Object.keys(req.body).filter(prop => prop !== 'id');
+
+  if (!requiredProperties.every(prop => bodyProperties.includes(prop)) || bodyProperties.length !== requiredProperties.length) {
+    return res.status(400).json({ error: 'Estructura incorrecta en el cuerpo de la solicitud' });
+  }
+
+  if (!nombre) {
+    return res.status(400).json({ error: 'El nombre de la categoría es obligatorio' });
+  }
+
+  // Generar un ID único
+  const uniqueId = generateUniqueId();
+
+  const newCategory = {
+    id: uniqueId,
+    nombre,
   };
+
+  try {
+    // Leer datos actuales de categorías desde el archivo
+    const categoriasData = fs.readFileSync(filePath3, 'utf-8');
+    let categorias = JSON.parse(categoriasData);
+
+    // Verificar si categorías es un array, si no, inicializar como array vacío
+    if (!Array.isArray(categorias)) {
+      categorias = [];
+    }
+
+    // Agregar la nueva categoría al array de categorías
+    categorias.push(newCategory);
+
+    // Escribir el array actualizado de categorías de nuevo al archivo
+    fs.writeFileSync(filePath3, JSON.stringify(categorias, null, 2));
+
+    // Responder con la nueva categoría añadida
+    res.status(201).json(newCategory);
+  } catch (error) {
+    console.error('Error al agregar la categoría:', error);
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+};
 
 // Actualizar una categoría por su ID
 exports.updateCategory = (req, res) => {
@@ -632,16 +638,17 @@ exports.getVentaById = (req, res) => {
   }
 };
 
+// Función para agregar una nueva venta
 exports.addVenta = (req, res) => {
   const { cliente, fechaVenta, productosVendidos } = req.body;
 
   // Validar los datos de la venta
   const requiredProperties = ['cliente', 'fechaVenta', 'productosVendidos'];
 
-  if (
-    !requiredProperties.every(prop => req.body.hasOwnProperty(prop)) ||
-    Object.keys(req.body).length !== requiredProperties.length
-  ) {
+  // Excluir 'id' de la validación
+  const bodyProperties = Object.keys(req.body).filter(prop => prop !== 'id');
+
+  if (!requiredProperties.every(prop => bodyProperties.includes(prop)) || bodyProperties.length !== requiredProperties.length) {
     return res.status(400).json({ error: 'Estructura incorrecta en el cuerpo de la solicitud' });
   }
 
@@ -649,13 +656,20 @@ exports.addVenta = (req, res) => {
     return res.status(400).json({ error: 'Datos incompletos para la venta' });
   }
 
-  const newVenta = new Venta(generateUniqueId(), cliente, fechaVenta, productosVendidos);
+  // Generar un ID único
+  const uniqueId = generateUniqueId();
 
-  console.log('Ruta de compra.json:', filePath4);
-  console.log('Ruta de venta.json:', filePath5);
+  const newVenta = {
+    id: uniqueId,
+    cliente,
+    fechaVenta,
+    productosVendidos,
+  };
 
   try {
+    // Lógica para agregar la nueva venta al archivo o base de datos
     addData(filePath5, newVenta);
+
     res.status(201).json(newVenta);
   } catch (error) {
     console.error('Error al agregar venta:', error);
