b7eb7be2aa1c547c2cf5de75466aa37a16331794
hervey14 <hervey146@gmail.com>
2023-11-30 12:00:32 -0700

    Productos corregido no creaba 30/11/2023

--- ProyectoBackyFront/Backend/APIMongoDBConInterfazV2/routes/categorias.js
+++ ProyectoBackyFront/Backend/APIMongoDBConInterfazV2/routes/categorias.js
@@ -7,7 +7,7 @@ router.get('/categoria', categoriaController.getAllCategories);
 router.post('/tabla/agregar', categoriaController.addCategory);
 router.get('/categoria/:id', categoriaController.getCategoryById);
 router.put('/categoria/:id', categoriaController.updateCategory);
-router.delete('/categoria/:id', categoriaController.deleteCategory);
+router.delete('/eliminar/:id', categoriaController.deleteCategory);
 router.get('/tablacategorias', categoriaController.renderTablaCategorias);
 router.get('/editarCategoria/:id', categoriaController.editCategoryPage);
 
